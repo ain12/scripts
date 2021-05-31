34898a03d9c68f844386d471552fa20392d21664
Ainhoa <ainhoaactic@gmail.com>
2021-05-31 10:31:49 +0200

    level 1 - show text on screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Line from "./components/Line.js";
+import script from "./assets/script.json";
 
 function App() {
+  const FullScript = script.map(line => <Line key={line.id} line={line} />);
   return (
     <div className="App">
-
+      {FullScript}
     </div>
   );
 }
---
+++ src/components/Line.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Line = (props) => {
+    return (
+        <div>
+            <p>{props.line}</p>
+        </div>
+    )
+}
+
+export default Line;
