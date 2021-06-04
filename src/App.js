5898baf47ba27e19721ee4c4c59cc91f2b97eed4
Ainhoa <ainhoaactic@gmail.com>
2021-06-04 17:40:59 +0200

    level 2- style with styled-components and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import { BoxDiv, BodyDiv } from './styles.js';
 import Line from "./components/Line.js";
 import script from "./assets/script.json";
+import "./styles.css";
 
 function App() {
   const FullScript = script.map(line => <Line key={line.id} line={line} />);
   return (
-    <div className="App">
-      {FullScript}
-    </div>
+
+    <BodyDiv>
+      <BoxDiv>
+        <h1 className="title">SCRIPT</h1>
+        {FullScript}
+      </BoxDiv>
+    </BodyDiv>
+
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const BodyDiv = styled.div`
+  box-sizing: border-box;
+  height: 100vh; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #FFF6FF;
+  font-family: 'Montserrat', sans-serif;
+`;
+
+export const BoxDiv = styled.div`
+  text-align: center;
+  padding: 30px 40px;
+  background-color: #A187B0;
+  color: #fff;
+  border-radius: 2rem;
+  -webkit-box-shadow: 0px 1px 11px 5px rgba(86,86,86,0.3); 
+  box-shadow: 0px 1px 11px 5px rgba(86,86,86,0.3);
+`;
