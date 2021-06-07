28becd3f29c23340aefe26d4aa5c4c8e93efe71d
Ainhoa <ainhoaactic@gmail.com>
2021-06-07 11:46:15 +0200

    level 3 ex: 3 - add two buttons (previous & next) to show only one sentence at a time

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { BoxDiv, BodyDiv } from './styles.js';
+import { BoxDiv, BodyDiv, Btn } from './styles.js';
 import Line from "./components/Line.js";
 import script from "./assets/script.json";
 import "./styles.css";
+import { useState } from 'react';
 
 function App() {
-  const FullScript = script.map(line => <Line key={line.id} line={line} />);
+  const [currentLine, setCurrentLine] = useState(0);
+
+  const nextBtn = () => {
+    return currentLine !== script.length - 1 && setCurrentLine(currentLine + 1);
+  }
+
+  const previousBtn = () => {
+    return currentLine !== 0 && setCurrentLine(currentLine - 1);
+  }
   return (
 
     <BodyDiv>
       <BoxDiv>
         <h1 className="title">SCRIPT</h1>
-        {FullScript}
+        <h3>Click the buttons to start reading the script.</h3>
+        <Btn onClick={previousBtn}>Previous</Btn>
+        <Btn onClick={nextBtn}>Next</Btn>
+        <Line line={script[currentLine]}></Line>
       </BoxDiv>
     </BodyDiv>
-
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const BodyDiv = styled.div`
   box-sizing: border-box;
+  overflow-x: hidden;
   height: 100vh; 
   display: flex;
   justify-content: center;
@@ -15,7 +16,30 @@ export const BoxDiv = styled.div`
   padding: 30px 40px;
   background-color: #A187B0;
   color: #fff;
+  width: 70%;
   border-radius: 2rem;
   -webkit-box-shadow: 0px 1px 11px 5px rgba(86,86,86,0.3); 
   box-shadow: 0px 1px 11px 5px rgba(86,86,86,0.3);
+`;
+
+export const Btn = styled.button`
+ display: inline-block;
+ padding: 0.8rem;
+ margin: 0.5rem;
+ width: 9rem;
+ border-radius: 2em;
+ border: 2px solid #FEF7FF;
+ box-sizing: border-box;
+ text-decoration: none;
+ background-color: #4D4352;
+ font-weight:300;
+ color:#fff;
+ font-size: 16px;
+ text-align: center;
+ transition: all 0.2s;
+ 
+ &:hover {
+   border-color: #FEFEDF;
+   cursor: pointer;
+ }
 `;
